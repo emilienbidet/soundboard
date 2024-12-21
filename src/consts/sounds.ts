77dbd0552381409c5aee101bc263ab12ecc53cac
Emilien Bidet <emilienbdt@gmail.com>
2024-12-21 12:02:41 +0100

    Add more sounds

--- src/consts/sounds.ts
+++ src/consts/sounds.ts
@@ -1,13 +1,45 @@
 import carelessWhispers from "@/assets/sounds/careless-whispers.mp3";
-import fart from "@/assets/sounds/fart.mp3";
+import danger from "@/assets/sounds/danger.mp3";
+import fart1 from "@/assets/sounds/fart-1.mp3";
 import fart2 from "@/assets/sounds/fart-2.mp3";
-import villager from "@/assets/sounds/villager.mp3";
+import fart3 from "@/assets/sounds/fart-3.mp3";
+import fart4 from "@/assets/sounds/fart-4.mp3";
+import frenchMeme from "@/assets/sounds/french-meme.mp3";
+import leJustePrixGenerique from "@/assets/sounds/le-juste-prix-generique.mp3";
+import minecraftDragon from "@/assets/sounds/minecraft-dragon.mp3";
+import questionPourUnChampionGenerique from "@/assets/sounds/question-pour-un-champion-generique.mp3";
+import roblox from "@/assets/sounds/roblox.mp3";
+import spongebobFail from "@/assets/sounds/spongebob-fail.mp3";
+import tension from "@/assets/sounds/tension.mp3";
+import villager1 from "@/assets/sounds/villager-1.mp3";
 import villager2 from "@/assets/sounds/villager-2.mp3";
+import villager3 from "@/assets/sounds/villager-3.mp3";
+import wrongAnswer from "@/assets/sounds/wrong-answer.mp3";
 
 export const sounds: { file: string; name: string; color: string }[] = [
 	{ file: carelessWhispers, name: "Careless Whispers", color: "red" },
-	{ file: fart, name: "Fart", color: "green" },
+	{ file: danger, name: "Danger", color: "red" },
+	{ file: fart1, name: "Fart 1", color: "green" },
 	{ file: fart2, name: "Fart 2", color: "green" },
-	{ file: villager, name: "Villager", color: "blue" },
+	{ file: fart3, name: "Fart 3", color: "green" },
+	{ file: fart4, name: "Fart 4", color: "green" },
+	{ file: frenchMeme, name: "French Meme", color: "blue" },
+	{
+		file: leJustePrixGenerique,
+		name: "Le Juste Prix Generique",
+		color: "blue",
+	},
+	{ file: minecraftDragon, name: "Minecraft Dragon", color: "blue" },
+	{
+		file: questionPourUnChampionGenerique,
+		name: "Question Pour Un Champion Generique",
+		color: "blue",
+	},
+	{ file: roblox, name: "Roblox", color: "blue" },
+	{ file: spongebobFail, name: "Spongebob Fail", color: "blue" },
+	{ file: tension, name: "Tension", color: "blue" },
+	{ file: villager1, name: "Villager", color: "blue" },
 	{ file: villager2, name: "Villager 2", color: "blue" },
+	{ file: villager3, name: "Villager 3", color: "blue" },
+	{ file: wrongAnswer, name: "Wrong Answer", color: "blue" },
 ];
